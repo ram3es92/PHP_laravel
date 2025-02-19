5704ee17b974156696cce1243a9ffd826ec4ec35
Роман Князев <ram3es92@mail.ru>
2025-02-19 20:40:02 +0300

    routes

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\FormProcessor;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/userform', [FormProcessor::class, 'index']);
+
+Route::post('/store_form', [FormProcessor::class, 'store']);
