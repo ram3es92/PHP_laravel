407804fc41c6a316fc6a3ab3223c6de3be9c5e06
Роман Князев <ram3es92@mail.ru>
2025-02-21 19:32:12 +0300

    добавил данные в контроллер

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,10 +1,43 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
 {
-    //
+    protected $employeesData = [
+        [
+            'name' => 'Roma',
+            'surname' => 'Knaz',
+            'email' => 'rom.knaz@example.com',
+        ],
+        [
+            'name' => 'Anna',
+            'surname' => 'Popova',
+            'email' => 'popova.anna@example.com',
+        ],
+    ];
+
+    public function __invoke()
+    { {
+          
+            if (Employee::count() === 0) {
+                
+                foreach ($this->employeesData as $data) {
+                    $employee = new Employee();
+                    $employee->name = $data['name'];
+                    $employee->surname = $data['surname'];
+                    $employee->email = $data['email'];
+                    $employee->save();
+                }
+            }
+
+           
+            $employees = Employee::all();
+
+            
+            return view('employees', compact('employees'));
+        }
+    }
 }
