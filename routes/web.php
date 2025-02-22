4e098959fe2b6dd1f34f1ef2e7cf2c3225e1e24a
Роман Князев <ram3es92@mail.ru>
2025-02-22 13:24:53 +0300

    изменил роуты в web.php добавил book

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\BookController;
 
 Route::get('/', function () {
     return view('form');
 });
 
-Route::get('get-employee-data', [EmployeeController::class, 'index'])->name('form');
+//Route::get('get-employee-data', [EmployeeController::class, 'index'])->name('form');
 
 Route::post('/store_form', [EmployeeController::class, 'store']);
 
 Route::put('/employee/{id}', [EmployeeController::class, 'update']);
 
 Route::get('/data', [EmployeeController::class, 'saveApiData']);
+
+Route::get('/book{id?}', [BookController::class, 'index'])->name('show_book');
+
+Route::post('/book', [BookController::class, 'store'])->name('store_book');
