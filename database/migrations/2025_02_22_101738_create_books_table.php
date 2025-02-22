fbde7bbb7b410e83b75fba704bf87c20dbe9b01c
Роман Князев <ram3es92@mail.ru>
2025-02-22 13:23:10 +0300

    изменил метод up в файле миграции booktables

--- database/migrations/2025_02_22_101738_create_books_table.php
+++ database/migrations/2025_02_22_101738_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->string('author');
+            $table->string('genre');
             $table->timestamps();
         });
     }
