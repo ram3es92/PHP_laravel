3f42f18fd0250558b1b346a4c4116f9163202157
Роман Князев <ram3es92@mail.ru>
2025-02-22 12:32:05 +0300

    изменил роуты в web.php

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-use App\Http\Controllers\FormProcessor;
-
-use App\Http\Controllers\EmployeeController;
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('home' , [
+        'name' => 'John',
+        'age' => 10,
+        'position' => 'Two',
+        'address' => 'Mellow street',
+    ]);
 });
 
-Route::get('/userform', [FormProcessor::class, 'index']);
-
-Route::post('/store_form', [FormProcessor::class, 'store']);
-
-Route::get('/hello/{firstname}', [FormProcessor::class, 'hello'])->name('hello');
-
-Route::get('/test_database', EmployeeController::class);
+Route::get('/contacts', function () {
+    return view('contacts' , [
+        'email' => '',
+        'post_code' => 654000,
+        'phone' => '88-32-17',
+        'address' => 'Mellow street',
+    ]);
+});
